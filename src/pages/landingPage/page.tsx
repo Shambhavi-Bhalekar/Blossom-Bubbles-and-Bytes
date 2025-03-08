6da1801cc6baefa4203e7a15fff3fbf5ccbec08a
Shambhavi-Bhalekar <bhalekarshambhavi.15@gmail.com>
2025-03-08 09:33:36 +0530

    initial commit

---
+++ src/pages/landingPage/page.tsx
@@ -0,0 +1,12 @@
+import Aurora from '@/src/blocks/Backgrounds/Aurora/Aurora'
+import React from 'react'
+
+const landingPage = () => {
+  return (
+    <div>
+        <Aurora/>
+    </div>
+  )
+}
+
+export default landingPage
